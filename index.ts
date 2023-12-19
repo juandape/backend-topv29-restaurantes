4011224f99118c110f8972f0f4ab6cf454bbe3ff
Juan David Peña <juandavidp76@hotmail.com>
2023-12-19 12:58:07 -0500

    change

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
+require('dotenv').config();
 
 import { server } from './app';
 
+
 function startServer() {
-    const PORT = (process.env.PORT as string) || 3030;
+  const PORT = (process.env.PORT as string) || 3030;
 
-    server.listen(PORT, () => {
-      console.info(`Server running 🤖🚀 at http://localhost:${PORT}`);
-    });
-  }
+  server.listen(PORT, () => {
+    console.info(`Server running 🤖🚀 at http://localhost:${PORT}`);
+  });
+}
 
-  setImmediate(startServer);
-  export default server;
+setImmediate(startServer);
+export default server;
